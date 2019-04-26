359ee57459096c5283e7bb96c2f85fbd944a8900
esra emad <esso.em.96@gmail.com>
2019-04-26 20:34:46 +0200

    third work

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$(function(){
+    'use strict';
+    
+//    $('.change-cont > div').hide();
+//    $($(this).data('class')).show();
+    
+    $('.box-feat').click( function(){
+            $('.change-cont > div').hide();
+            $($(this).data('class')).show();
+    })
+        
+
+    
+});
